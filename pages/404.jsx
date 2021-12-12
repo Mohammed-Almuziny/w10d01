8a759d22a5124ea9237ddb4239d0f057891546ac
Mohammed-Almuziny <mohammed.abdullah.js@tuwaiq.edu.sa>
2021-12-12 15:15:47 +0300

    update 404 page not found page

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,5 +1,20 @@
+import { useRouter } from "next/router";
+
 const NotFound = () => {
-  return <h1>Page Not Found</h1>;
+  const router = useRouter();
+
+  setTimeout(() => {
+    router.back();
+  }, 3000);
+
+  return (
+    <>
+      <h1>Page Not Found</h1>
+      <h3>back to previous page in 3 seconds</h3>
+
+      <button onClick={() => router.push("/")}>go back home</button>
+    </>
+  );
 };
 
 export default NotFound;
